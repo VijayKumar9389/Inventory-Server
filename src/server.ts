a2628da9c90952553ad51cc4bde47dc939aeac85
VijayKumar9389 <vijaymkumar2095@gmail.com>
2024-05-26 17:38:46 -0600

    Fix deployment

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(
         origin: process.env.ORIGIN,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'refreshToken', 'accessToken'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin', 'refreshToken', 'accessToken'],
         exposedHeaders: ['Authorization'],
     })
 );
